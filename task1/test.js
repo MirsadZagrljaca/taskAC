4cce1172232dc0643d39f11752cd6a7eeb89b6c8
MirsadZagrljaca <mzagrljaca@gmail.com>
2022-02-20 19:26:56 +0100

    Task 1 -- Adding more tests

--- task1/test.js
+++ task1/test.js
@@ -33,6 +33,21 @@ describe("Testing the algorithm", () => {
     assert.equal(c.value, 6);
   });
 
+  it("should return a new pair with value 0, if conditions are not met", () => {
+    let a = [
+      { id: 1, value: 3 },
+      { id: 2, value: 7 },
+      { id: 3, value: 1 },
+      { id: 4, value: 5 },
+      { id: 5, value: 4 },
+    ];
+
+    let b = getNewStruct(a);
+    let c = b[b.length - 1];
+    assert.equal(c.id, 6);
+    assert.equal(c.value, 0);
+  });
+
   it("handling wrong object types:string", () => {
     let b = getNewStruct("Sending string");
     assert.equal(b, "Not an Object!");
